0b7cf1c2bf051bbf104abd13281bc0b8f2c4c843
github username <github email address>
2021-03-28 18:25:31 -0400

    creates new Truck via new_truck_path

--- app/controllers/trucks_controller.rb
+++ app/controllers/trucks_controller.rb
@@ -4,11 +4,23 @@ class TrucksController < ApplicationController
     end
 
     def create
-        binding.pry
-        redirect_to trucks_path
+        truck = Truck.new(truck_params)
+        if truck.save
+            session[:truck_id] = truck.id
+            redirect_to trucks_path
+        else
+            redirect_to new_truck_path
+        end
     end
 
     def index
+        @trucks = Truck.all
+    end
+
+    private
+
+    def truck_params
+        params.require(:truck).permit(:name, :email, :password, :zipcode)
     end
 
 end
