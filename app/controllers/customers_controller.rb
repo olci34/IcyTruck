833f2820163aa0b50073ffeb3aaf8abdbbe94ee0
github username <github email address>
2021-03-29 13:38:36 -0400

    creates Customers in /signup

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -5,14 +5,27 @@ class CustomersController < ApplicationController
     end
 
     def create
-
+        customer = Customer.new(customer_params)
+        if customer.save
+            session[:customer_id] = customer.id
+            redirect_to customers_path
+        else
+            redirect_to signup_path, alert: "Invalid email or password"
+        end
     end
 
     def index
-        @trucks = Truck.where()
+        @trucks = Truck.all
     end
 
     def show
+    end
+
+
+    private
+    
+    def customer_params
+        params.require(:customer).permit(:name,:email,:password,:zipcode)
     end
 
 end
