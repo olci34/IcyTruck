dda7d34e46e0fd86da2a988167dd2c28aa614e4b
github username <github email address>
2021-03-31 17:44:13 -0400

    adds new validation to Order

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,12 +3,12 @@ class Order < ApplicationRecord
     belongs_to :truck
     has_many :icecreams,through: :icecreams_orders
     has_many :icecreams_orders
-    validate :order_valid?
+    validate :order_valid?, :order_total_valid?
     # accepts_nested_attributes_for :icecreams_orders
     
     def icecreams_orders_attributes=(attributes)
         attributes.values.each do |attribute|
-            if !attribute[:quantity].strip.empty? ### had to add it because customer leaves empty quantities in form for
+            if !attribute[:quantity].strip.empty? && (attribute[:quantity].to_i != 0) ### had to add it because customer leaves empty quantities in form for
                 icecreams_order = IcecreamsOrder.create(attribute)
                 icecreams_order.calculate_total
                 self.icecreams_orders << icecreams_order
@@ -22,6 +22,10 @@ class Order < ApplicationRecord
 
     def order_valid?
         self.total <= self.customer.wallet || self.errors.add(:total, "is higher than your balance")
+    end
+
+    def order_total_valid?
+        update_total > 0.0 || self.errors.add(:total, "has to be greater than $0")
     end
 
 end
